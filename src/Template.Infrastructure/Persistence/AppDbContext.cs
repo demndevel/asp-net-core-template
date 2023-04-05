5838cebdd2b98f6416d576858634a4c9c42bbdfd
demndevel <demndevel@gmail.com>
2023-04-05 19:40:46 +0500

    add DbContextOptions for db context

--- src/Template.Infrastructure/Persistence/AppDbContext.cs
+++ src/Template.Infrastructure/Persistence/AppDbContext.cs
@@ -9,6 +9,10 @@ public class AppDbContext : DbContext, IAppDbContext
 {
     public DbSet<User> Users { get; set; } = null!;
 
+    public AppDbContext(DbContextOptions<AppDbContext> options) 
+        : base(options)
+    { }
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
